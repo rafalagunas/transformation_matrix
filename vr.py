95202c1efd021316ab33bd29ce01f54f12b3b857
rafastaria <guitronleafar@gmail.com>
2019-08-30 18:54:04 -0500

    updated

--- vr.py
+++ vr.py
@@ -3,30 +3,20 @@ import numpy as np
 
 #el ultimo uno de cada vector no cuenta no cuenta
 m_one = np.array([[1,2,1,1],[2,4,3,1],[2,4,3,1]])
-
 #Escalar en zoom +
 m_two = np.array([[4,0,0,0],[0,4,0,0],[0,0,4,0],[0,0,0,1]])
-
 # Escalar en zoom -
 m_three = np.array([[-0.4,0,0,0],[0,-0.4,0,0],[0,0,-0.4,1],[0,0,0,1]])
-
 #Mover izquierda
 m_four = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[-0.1,0,0,1]])
-
 #Mover derecha
 m_five = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[1,0,0,1]])
-
 #Mover abajo
 m_six = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,-0.1,0,1]])
-
 #Mover arriba
 m_seven =  np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,1,0,1]])
-
-
 #Acercar
 m_eight = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,1,1]])
-
-
 #Alejar
 m_nine =  np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,-0.1,1]])
 
